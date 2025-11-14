200edd16af878f7aa06f0f23d0e1690835f88256
Samukelo Jiyane <s.jiyane@saeon.nrf.ac.za>
2025-11-14 13:25:06 +0200

    Update base path in Vite configuration for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/climate-risk/',
+  base: '/climate-tool/',
   server: {
     host: 'localhost',
     port: 5173,

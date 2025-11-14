3549472531e47665e6bef31ace14ef31d85a711a
Samukelo Jiyane <s.jiyane@saeon.nrf.ac.za>
2025-11-14 13:12:36 +0200

    Set base path for production environment in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/climate-risk/' : '/',
+  base: '/climate-risk/',
   server: {
     host: 'localhost',
     port: 5173,
